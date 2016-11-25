ab9de6bed2da7cade85f84b3ea0f9bf9796db01b
Aaron Magid <ahm64@case.edu>
2016-11-25 19:26:43 +0200

    feat(app): Implemented new config and logger architecture.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 'use strict';
 
-const http = require('http')
-const app = require('express')()
-const server = http.createServer(app)
-const Promise = require('bluebird')
+const http = require('http');
+const app = require('express')();
+const server = http.createServer(app);
+const Promise = require('bluebird');
+const api = require('./api');
+const config = require('./config');
+const sync = require('./models/sync');
 
-const api = require('./api')
+// Do basic config stuff, like set up logger
+config.init();
 
-api();
+// Sync database models
+sync();
 
-app.listen(3000)
+app.listen(3000);
